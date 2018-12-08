96aee3e9c94e3a2fde6a798ee14ec9ce02229a04
stdevYuniers <dev0010@simelo.tech>
2018-12-08 17:15:39 -0500

    [ci] refs #130 Export GOARCH as global variable

--- .ci-scripts/install-deps.sh
+++ .ci-scripts/install-deps.sh
@@ -2,21 +2,22 @@
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update -qq
-    sudo apt-get install --no-install-recommends -y \
-                        libglib2.0-dev \
-                        gtk+3.0 \
-                        libgtk-3-dev \
-                        libpango1.0-dev \
-                        libcairo2-dev \
-                        libperl-dev \
-                        libgtk2.0-dev \
-                        gir1.2-gtk-3.0 \
-                        libxi-dev \
-                        libgl1-mesa-dev \
-                        libxrandr-dev \
-                        libxcursor-dev \
-                        libxinerama-dev \
-                        xvfb
+    sudo apt-get install -y \
+                libglib2.0-dev \
+                gtk+3.0 \
+                libgtk-3-dev \
+                libpango1.0-dev \
+                libcairo2-dev \
+                libperl-dev \
+                libgtk2.0-dev \
+                gir1.2-gtk-3.0 \
+                libxi-dev \
+                libgl1-mesa-dev \
+                libxrandr-dev \
+                libxcursor-dev \
+                libxinerama-dev \
+                xvfb \
+                --no-install-recommends
     export DISPLAY=:99.0
     sudo /usr/bin/Xvfb $DISPLAY 2>1 > /dev/null &
     export GTK_VERSION=$(pkg-config --modversion gtk+-3.0 | tr . _| cut -d '_' -f 1-2)
