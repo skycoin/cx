6d81b594184bd27753f2ae7202c817fbb3ff94d2
Olemis Lang <olemis+work@gmail.com>
2018-12-23 01:50:35 -0500

    [cxgo] refs #175 - Fix build errors in CX flags code
    reported in https://travis-ci.org/simelo/cx/jobs/471518130

--- cxgo/flags.go
+++ cxgo/flags.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"path/filepath"
 )
 
 type cxCmdFlags struct {
 	baseOutput          bool
 	compileMode         bool
 	compileOutput       string
-	initProject         bool
+	newProject          bool
 	replMode            bool
 	signalClientMode    bool
 	signalClientID      int
@@ -22,7 +24,7 @@ type cxCmdFlags struct {
 	interpretMode       bool
 }
 
-func defaultCmdFlags() cxCmdOptions {
+func defaultCmdFlags() cxCmdFlags {
 	return cxCmdFlags{
 		baseOutput:          false,
 		compileMode:         false,
@@ -46,18 +48,18 @@ func registerFlags(options *cxCmdFlags) {
 		options.replMode = true
 	}
 
-	flag.BoolVar(&printVersion, "version", printVersion, "Print CX version")
-	flag.BoolVar(&printVersion, "v", printVersion, "alias for -version")
-	flag.BoolVar(&printHelp, "help", printHelp, "Print CX version")
-	flag.BoolVar(&printHelp, "h", printHelp, "alias for -help")
+	flag.BoolVar(&options.printVersion, "version", options.printVersion, "Print CX version")
+	flag.BoolVar(&options.printVersion, "v", options.printVersion, "alias for -version")
+	flag.BoolVar(&options.printHelp, "help", options.printHelp, "Print CX version")
+	flag.BoolVar(&options.printHelp, "h", options.printHelp, "alias for -help")
 	flag.BoolVar(&options.baseOutput, "base", options.baseOutput, "generate a 'out.cx.go' file with the transcompiled CX Base source code.")
 	flag.BoolVar(&options.baseOutput, "b", options.baseOutput, "alias for -base")
 	flag.BoolVar(&options.compileMode, "compile", options.compileMode, "generate a 'out' executable file of the program")
 	flag.BoolVar(&options.compileMode, "c", options.compileMode, "alias for -compile")
-	flag.BoolVar(&options.compileOutput, "compile-output", options.compileOutput, "Specifies the filename for the generated executable.")
+	flag.StringVar(&options.compileOutput, "compile-output", options.compileOutput, "Specifies the filename for the generated executable.")
 	flag.StringVar(&options.compileOutput, "co", options.compileOutput, "alias for -compile-output")
-	flag.BoolVar(&options.initProject, "new", options.initProject, "Creates a new project located at $CXPATH/src")
-	flag.BoolVar(&options.initProject, "n", options.initProject, "alias for -new")
+	flag.BoolVar(&options.newProject, "new", options.newProject, "Creates a new project located at $CXPATH/src")
+	flag.BoolVar(&options.newProject, "n", options.newProject, "alias for -new")
 	flag.BoolVar(&options.replMode, "repl", options.replMode, "Loads source files into memory and starts a read-eval-print loop")
 	flag.BoolVar(&options.replMode, "r", options.replMode, "alias for -repl")
 	flag.BoolVar(&options.webMode, "web", options.webMode, "Start CX as a web service.")
@@ -66,7 +68,7 @@ func registerFlags(options *cxCmdFlags) {
 	// viscript options
 	flag.BoolVar(&options.signalClientMode, "signal-client", options.signalClientMode, "Run signal client")
 	flag.IntVar(&options.signalClientID, "signal-client-id", options.signalClientID, "Id of signal client (default 1)")
-	flag.StringVar(&options.signalSeverAddress, "signal-client-address", options.signalSeverAddress, "Address of signal server (default 'localhost:7999')")
+	flag.StringVar(&options.signalServerAddress, "signal-client-address", options.signalServerAddress, "Address of signal server (default 'localhost:7999')")
 }
 
 func printHelp() {
@@ -94,67 +96,61 @@ Notes:
 }
 
 func parseArgsForCX(args []string) (cxArgs []string, sourceCode []*os.File, fileNames []string) {
-	for i, arg := range args {
+	for _, arg := range args {
 		if len(arg) > 2 && arg[:2] == "++" {
 			cxArgs = append(cxArgs, arg)
 			continue
 		}
-		if !flagMode {
+		fi, err := os.Stat(arg)
+		_ = err
 
-			fi, err := os.Stat(arg)
-			_ = err
+		if err != nil {
+			panic(err)
+		}
+
+		switch mode := fi.Mode(); {
+		case mode.IsDir():
+			var fileList []string
+
+			err := filepath.Walk(arg, func(path string, f os.FileInfo, err error) error {
+				fileList = append(fileList, path)
+				return nil
+			})
 
 			if err != nil {
 				panic(err)
 			}
 
-			switch mode := fi.Mode(); {
-			case mode.IsDir():
-				var fileList []string
-
-				err := filepath.Walk(arg, func(path string, f os.FileInfo, err error) error {
-					fileList = append(fileList, path)
-					return nil
-				})
+			for _, path := range fileList {
+				file, err := os.Open(path)
 
 				if err != nil {
 					panic(err)
 				}
 
-				for _, path := range fileList {
-					file, err := os.Open(path)
+				fiName := file.Name()
+				fiNameLen := len(fiName)
 
-					if err != nil {
-						panic(err)
-					}
-
-					fiName := file.Name()
-					fiNameLen := len(fiName)
-
-					if fiNameLen > 2 && fiName[fiNameLen-3:] == ".cx" {
-						// only loading .cx files
-						sourceCode = append(sourceCode, file)
-						fileNames = append(fileNames, fiName)
-					}
+				if fiNameLen > 2 && fiName[fiNameLen-3:] == ".cx" {
+					// only loading .cx files
+					sourceCode = append(sourceCode, file)
+					fileNames = append(fileNames, fiName)
 				}
-			case mode.IsRegular():
-				file, err := os.Open(arg)
-
-				if err != nil {
-					panic(err)
-				}
-
-				fileNames = append(fileNames, file.Name())
-				sourceCode = append(sourceCode, file)
 			}
+		case mode.IsRegular():
+			file, err := os.Open(arg)
+
+			if err != nil {
+				panic(err)
+			}
+
+			fileNames = append(fileNames, file.Name())
+			sourceCode = append(sourceCode, file)
 		}
 	}
+	return
 }
 
 func printVersion() {
 	fmt.Println("CX version", VERSION)
-}
-
-func parseCxArgs(args []string) {
-	a
 }
