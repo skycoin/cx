83cd0d2b4be32d5aee450e94f77a4790927ea35e
Maykel Arias Torres <dev0006@simelo.tech>
2019-01-03 10:49:30 -0500

    refs #201 Code fixes in cx/cxstruct.go

--- cx/cxstruct.go
+++ cx/cxstruct.go
@@ -4,12 +4,13 @@ import (
 	// "errors"
 	"fmt"
 
-	. "github.com/satori/go.uuid"
+	. "github.com/satori/go.uuid" //nolint golint
 )
 
 /* The CXStruct struct contains information about a CX struct.
  */
 
+// CXStruct ...
 type CXStruct struct {
 	Fields    []*CXArgument
 	Name      string
@@ -18,6 +19,7 @@ type CXStruct struct {
 	ElementID UUID
 }
 
+// MakeStruct ...
 func MakeStruct(name string) *CXStruct {
 	return &CXStruct{
 		ElementID: MakeElementID(),
@@ -28,14 +30,16 @@ func MakeStruct(name string) *CXStruct {
 // ----------------------------------------------------------------
 //                             Getters
 
+// GetFields ...
 func (strct *CXStruct) GetFields() ([]*CXArgument, error) {
 	if strct.Fields != nil {
 		return strct.Fields, nil
-	} else {
-		return nil, fmt.Errorf("structure '%s' has no fields", strct.Name)
 	}
+	return nil, fmt.Errorf("structure '%s' has no fields", strct.Name)
+
 }
 
+// GetField ...
 func (strct *CXStruct) GetField(name string) (*CXArgument, error) {
 	for _, fld := range strct.Fields {
 		if fld.Name == name {
@@ -48,6 +52,7 @@ func (strct *CXStruct) GetField(name string) (*CXArgument, error) {
 // ----------------------------------------------------------------
 //                     Member handling
 
+// AddField ...
 func (strct *CXStruct) AddField(fld *CXArgument) *CXStruct {
 	found := false
 	for _, fl := range strct.Fields {
@@ -63,6 +68,7 @@ func (strct *CXStruct) AddField(fld *CXArgument) *CXStruct {
 	return strct
 }
 
+// RemoveField ...
 func (strct *CXStruct) RemoveField(fldName string) {
 	if len(strct.Fields) > 0 {
 		lenFlds := len(strct.Fields)
