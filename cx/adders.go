c8364fceb44245ccaa3726d89ae27b1008c935d4
Inge Wallin <inge@lysator.liu.se>
2019-01-08 18:29:05 +0100

    Collect CXArgument definition and methods into one file.

--- cx/adders.go
+++ cx/adders.go
@@ -3,17 +3,3 @@ package base
 // "fmt"
 // "github.com/skycoin/skycoin/src/cipher/encoder"
 
-// AddType ...
-func (arg *CXArgument) AddType(typ string) *CXArgument {
-	// arg.Typ = typ
-	if typCode, found := TypeCodes[typ]; found {
-		arg.Type = typCode
-		size := GetArgSize(typCode)
-		arg.Size = size
-		arg.TotalSize = size
-	} else {
-		arg.Type = TYPE_UNDEFINED
-	}
-
-	return arg
-}
--- cx/structs.go
+++ cx/structs.go
@@ -1,9 +1,5 @@
 package base
 
-import (
-	. "github.com/satori/go.uuid" //nolint golint
-)
-
 // CXCall ...
 type CXCall struct {
 	Operator     *CXFunction
@@ -23,36 +19,3 @@ type CXConstant struct {
 	Type  int
 }
 
-// CXArgument ...
-type CXArgument struct {
-	Lengths               []int // declared lengths at compile time
-	DereferenceOperations []int // offset by array index, struct field, pointer
-	DeclarationSpecifiers []int // used to determine finalSize
-	Indexes               []*CXArgument
-	Fields                []*CXArgument // strct.fld1.fld2().fld3
-	Name                  string
-	FileName              string
-	ElementID             UUID
-	Type                  int
-	Size                  int // size of underlaying basic type
-	TotalSize             int // total size of an array, performance reasons
-	Offset                int
-	IndirectionLevels     int
-	DereferenceLevels     int
-	PassBy                int // pass by value or reference
-	FileLine              int
-	CustomType            *CXStruct
-	Package               *CXPackage
-	IsSlice               bool
-	IsArray               bool
-	IsArrayFirst          bool // and then dereference
-	IsPointer             bool
-	IsReference           bool
-	IsDereferenceFirst    bool // and then array
-	IsStruct              bool
-	IsRest                bool // pkg.var <- var is rest
-	IsLocalDeclaration    bool
-	IsShortDeclaration    bool
-	PreviouslyDeclared    bool
-	DoesEscape            bool
-}
