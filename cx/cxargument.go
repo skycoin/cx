c3636d3afeafcdf0153fb58ac20fd490c2508aab
Amaury Hernandez <amherag@gmail.com>
2019-01-10 22:45:32 -0800

    AddType was different than develop's and it was causing an error

--- cx/cxargument.go
+++ cx/cxargument.go
@@ -85,12 +85,12 @@ func MakeGlobal(name string, typ int, fileName string, fileLine int) *CXArgument
 
 // AddType ...
 func (arg *CXArgument) AddType(typ string) *CXArgument {
-	// arg.Typ = typ
 	if typCode, found := TypeCodes[typ]; found {
 		arg.Type = typCode
 		size := GetArgSize(typCode)
 		arg.Size = size
 		arg.TotalSize = size
+		arg.DeclarationSpecifiers = append(arg.DeclarationSpecifiers, DECL_BASIC)
 	} else {
 		arg.Type = TYPE_UNDEFINED
 	}
