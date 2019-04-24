6c3400d2cd0f704cc020dc29cc18c7787b3ffff8
Amaury Hernandez <amherag@gmail.com>
2019-04-23 20:00:00 -0700

    Added documentation to CXProgram's fields

--- cx/cxprogram.go
+++ cx/cxprogram.go
@@ -23,33 +23,33 @@ import (
 //
 type CXProgram struct {
 	// Metadata
-	Path      string
-	ElementID UUID
+	Path      string // Path to the CX project in the filesystem
+	ElementID UUID // Was supposed to be used for blockchain integration. Needs to be removed.
 
 	// Contents
-	Packages []*CXPackage
+	Packages []*CXPackage // Packages in a CX program
 
 	// Runtime information
-	Inputs       []*CXArgument
-	Outputs      []*CXArgument
+	Inputs       []*CXArgument // OS input arguments
+	Outputs      []*CXArgument // outputs to the OS
 	Memory       []byte // Used when running the program
-	StackSize    int
-	HeapStartsAt int
-	StackPointer int
-	CallStack    []CXCall
-	CallCounter  int
-	HeapPointer  int
-	Terminated   bool
+	StackSize    int // This field stores the size of a CX program's stack
+	HeapStartsAt int // Offset at which the heap starts in a CX program's memory
+	StackPointer int // At what byte the current stack frame is
+	CallStack    []CXCall // Collection of function calls
+	CallCounter  int // What function call is the currently being executed in the CallStack
+	HeapPointer  int // At what offset a CX program can insert a new object to the heap
+	Terminated   bool // Utility field for the runtime. Indicates if a CX program has already finished or not.
 
 	// Used by the REPL and parser
-	CurrentPackage *CXPackage
+	CurrentPackage *CXPackage // Represents the currently active package in the REPL or when parsing a CX file.
 }
 
 // CXCall ...
 type CXCall struct {
-	Operator     *CXFunction
-	Line         int
-	FramePointer int
+	Operator     *CXFunction // What CX function will be called when running this CXCall in the runtime
+	Line         int // What line in the CX function is currently being executed
+	FramePointer int // Where in the stack is this function call's local variables stored
 }
 
 // MakeProgram ...
