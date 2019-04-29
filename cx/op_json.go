22c2753cfd7ef22cab139b2014c8ea8636bdd6ea
asahi3g <totoyuyu246@gmail.com>
2019-04-30 00:20:25 +0200

    Add json bindings

---
+++ cx/op_json.go
@@ -0,0 +1,164 @@
+// +build base extra full
+
+package cxcore
+
+import (
+	//"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	//"reflect"
+	"strings"
+)
+
+const (
+	JSON_TOKEN_NULL = iota
+	JSON_TOKEN_DELIM
+	JSON_TOKEN_BOOL
+	JSON_TOKEN_F64
+	JSON_TOKEN_NUMBER
+	JSON_TOKEN_STR
+)
+
+type JSONFile struct {
+	file *os.File
+	//reader     *bufio.Reader
+	reader      *strings.Reader
+	decoder     *json.Decoder
+	token       interface{}
+	tokenType   int32
+	tokenDelim  json.Delim
+	tokenBool   bool
+	tokenF64    float64
+	tokenNumber json.Number
+	tokenStr    string
+}
+
+var jsons []JSONFile
+
+func opJSONOpen(expr *CXExpression, fp int) {
+	path := ReadStr(fp, expr.Inputs[0])
+	fmt.Printf("PATH %s\n", path)
+	/*file, err := os.Open(ReadStr(fp, expr.Inputs[0]))
+	if err != nil {
+		panic(err)
+	}*/
+
+	text, _ := ioutil.ReadFile(path)
+	reader := strings.NewReader(string(text))
+	///reader := bufio.NewReader(file)
+	var jsonFile JSONFile
+	//jsonFile.file = file
+	jsonFile.reader = reader
+	jsonFile.decoder = json.NewDecoder(jsonFile.reader)
+	jsonFile.decoder.UseNumber()
+	WriteMemory(GetFinalOffset(fp, expr.Outputs[0]), FromI32(int32(len(jsons))))
+	jsons = append(jsons, jsonFile)
+}
+
+func opJSONClose(expr *CXExpression, fp int) {
+	//jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	//jsonFile.file.Close()
+}
+
+func opJSONTokenMore(expr *CXExpression, fp int) {
+	jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	more := jsonFile.decoder.More()
+	WriteMemory(GetFinalOffset(fp, expr.Outputs[0]), FromBool(more))
+}
+
+func opJSONTokenNext(expr *CXExpression, fp int) {
+	index := ReadI32(fp, expr.Inputs[0])
+	var tokenType int32 = JSON_TOKEN_NULL
+
+	token, err := jsons[index].decoder.Token()
+	if err == io.EOF {
+	} else if err != nil {
+		panic(err)
+	} else {
+		jsons[index].token = token
+		switch value := token.(type) {
+		case json.Delim:
+			tokenType = JSON_TOKEN_DELIM
+			jsons[index].tokenDelim = value
+		case bool:
+			tokenType = JSON_TOKEN_BOOL
+			jsons[index].tokenBool = value
+		case float64:
+			tokenType = JSON_TOKEN_F64
+			jsons[index].tokenF64 = value
+		case json.Number:
+			tokenType = JSON_TOKEN_NUMBER
+			jsons[index].tokenNumber = value
+		case string:
+			tokenType = JSON_TOKEN_STR
+			jsons[index].tokenStr = value
+		default:
+			tokenType = JSON_TOKEN_NULL
+		}
+	}
+	jsons[index].tokenType = tokenType
+}
+
+func opJSONTokenType(expr *CXExpression, fp int) {
+	jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	WriteMemory(GetFinalOffset(fp, expr.Outputs[0]), FromI32(jsonFile.tokenType))
+}
+
+func opJSONTokenDelim(expr *CXExpression, fp int) {
+	jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	if jsonFile.tokenType != JSON_TOKEN_DELIM {
+		panic("json : not a delim value")
+	}
+	WriteMemory(GetFinalOffset(fp, expr.Outputs[0]), FromI32(int32(jsonFile.tokenDelim)))
+}
+
+func opJSONTokenBool(expr *CXExpression, fp int) {
+	jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	if jsonFile.tokenType != JSON_TOKEN_BOOL {
+		panic("json : not a bool value")
+	}
+	WriteMemory(GetFinalOffset(fp, expr.Outputs[0]), FromBool(jsonFile.tokenBool))
+}
+
+func opJSONTokenF64(expr *CXExpression, fp int) {
+	jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	var value float64
+	if jsonFile.tokenType == JSON_TOKEN_F64 {
+		value = jsonFile.tokenF64
+	} else if jsonFile.tokenType == JSON_TOKEN_NUMBER {
+		var err error
+		value, err = jsonFile.tokenNumber.Float64()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		panic("json : not a f64 value")
+	}
+	WriteMemory(GetFinalOffset(fp, expr.Outputs[0]), FromF64(value))
+}
+
+func opJSONTokenI64(expr *CXExpression, fp int) {
+	jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	var value int64
+	if jsonFile.tokenType == JSON_TOKEN_NUMBER {
+		var err error
+		value, err = jsonFile.tokenNumber.Int64()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		panic("json : not an int64 value")
+	}
+	WriteMemory(GetFinalOffset(fp, expr.Outputs[0]), FromI64(value))
+}
+
+func opJSONTokenStr(expr *CXExpression, fp int) {
+	jsonFile := jsons[ReadI32(fp, expr.Inputs[0])]
+	if jsonFile.tokenType != JSON_TOKEN_STR {
+		panic("json : not a string value")
+	}
+	WriteObject(GetFinalOffset(fp, expr.Outputs[0]), FromStr(jsonFile.tokenStr))
+}
--- cx/opcodes_base.go
+++ cx/opcodes_base.go
@@ -20,6 +20,18 @@ const (
 	OP_OS_RUN
 	OP_OS_EXIT
 
+	// json
+	OP_JSON_OPEN
+	OP_JSON_CLOSE
+	OP_JSON_TOKEN_MORE
+	OP_JSON_TOKEN_NEXT
+	OP_JSON_TOKEN_TYPE
+	OP_JSON_TOKEN_DELIM
+	OP_JSON_TOKEN_BOOL
+	OP_JSON_TOKEN_F64
+	OP_JSON_TOKEN_I64
+	OP_JSON_TOKEN_STR
+
 	// http
 	OP_HTTP_GET
 
@@ -65,6 +77,38 @@ func init() {
 		[]*CXArgument{newOpPar(TYPE_I32, false)},
 		[]*CXArgument{})
 
+	// json
+	AddOpCode(OP_JSON_OPEN, "json.Open",
+		[]*CXArgument{newOpPar(TYPE_STR, false)},
+		[]*CXArgument{newOpPar(TYPE_I32, false)})
+	AddOpCode(OP_JSON_CLOSE, "json.Close",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{})
+	AddOpCode(OP_JSON_TOKEN_MORE, "json.More",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{newOpPar(TYPE_BOOL, false)})
+	AddOpCode(OP_JSON_TOKEN_NEXT, "json.Next",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{})
+	AddOpCode(OP_JSON_TOKEN_TYPE, "json.Type",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{newOpPar(TYPE_I32, false)})
+	AddOpCode(OP_JSON_TOKEN_DELIM, "json.Delim",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{newOpPar(TYPE_I32, false)})
+	AddOpCode(OP_JSON_TOKEN_BOOL, "json.Bool",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{newOpPar(TYPE_BOOL, false)})
+	AddOpCode(OP_JSON_TOKEN_F64, "json.Float64",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{newOpPar(TYPE_F64, false)})
+	AddOpCode(OP_JSON_TOKEN_I64, "json.Int64",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{newOpPar(TYPE_I64, false)})
+	AddOpCode(OP_JSON_TOKEN_STR, "json.Str",
+		[]*CXArgument{newOpPar(TYPE_I32, false)},
+		[]*CXArgument{newOpPar(TYPE_STR, false)})
+
 	// exec
 	execNativeBase = func(prgrm *CXProgram) {
 		defer RuntimeError()
@@ -102,6 +146,27 @@ func init() {
 			case OP_OS_EXIT:
 				op_os_Exit(expr, fp)
 
+				// json
+			case OP_JSON_OPEN:
+				opJSONOpen(expr, fp)
+			case OP_JSON_CLOSE:
+				opJSONClose(expr, fp)
+			case OP_JSON_TOKEN_MORE:
+				opJSONTokenMore(expr, fp)
+			case OP_JSON_TOKEN_NEXT:
+				opJSONTokenNext(expr, fp)
+			case OP_JSON_TOKEN_TYPE:
+				opJSONTokenType(expr, fp)
+			case OP_JSON_TOKEN_DELIM:
+				opJSONTokenDelim(expr, fp)
+			case OP_JSON_TOKEN_BOOL:
+				opJSONTokenBool(expr, fp)
+			case OP_JSON_TOKEN_F64:
+				opJSONTokenF64(expr, fp)
+			case OP_JSON_TOKEN_I64:
+				opJSONTokenI64(expr, fp)
+			case OP_JSON_TOKEN_STR:
+				opJSONTokenStr(expr, fp)
 			default:
 				// DumpOpCodes(opCode)
 				panic("invalid base opcode")
