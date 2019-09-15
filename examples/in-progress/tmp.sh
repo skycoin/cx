349965427096b131013983340cbb48202827353e
canhdoan <thanhcanhdoan@gmail.com>
2019-09-15 16:56:45 +0700

    Update directory for examples from rosettacode.org

---
+++ examples/in-progress/tmp.sh
@@ -0,0 +1,10 @@
+
+mv -f perfect-number.cx ../rosettacode-examples/
+mv -f pythagorean.cx ../rosettacode-examples/
+mv -f quick-select.cx ../rosettacode-examples/
+mv -f selection-sort.cx ../rosettacode-examples/
+mv -f shell-sort.cx ../rosettacode-examples/
+mv -f stooge-sort.cx ../rosettacode-examples/
+mv -f van-der-corput.cx ../rosettacode-examples/
+mv -f van-eck-sequen.cx ../rosettacode-examples/
+mv -f vector-products.cx ../rosettacode-examples/
