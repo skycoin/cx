261143e1f5013ba3889737d8ea3218e147ba7ed2
Amaury Hernandez <amherag@gmail.com>
2020-03-26 18:12:37 -0700

    Added initial cipher library.

--- cx/opcodes_core.go
+++ cx/opcodes_core.go
@@ -9,7 +9,7 @@ package cxcore
 var CorePackages = []string{
 	// temporary solution until we can implement these packages in pure CX I guess
 	"al", "gl", "glfw", "time", "http", "os", "explorer", "aff",
-	"gltext", "cx", "json", "regexp",
+	"gltext", "cx", "json", "regexp", "cipher",
 }
 
 // op codes
