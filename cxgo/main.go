52e435493f5391695c6435aa18e180bd006e494c
SkycoinSynth <Synth@Skycoin.com>
2021-02-26 15:35:48 +0800

    deleting unused functions

--- cxgo/main.go
+++ cxgo/main.go
@@ -209,23 +209,6 @@ func Run(args []string) {
 	// or by setting the `--cxpath` flag.
 	checkCXPathSet(options)
 
-	// Does the user want to run a CX publisher or peer node?
-	if options.publisherMode || options.peerMode {
-		optionRunNode(options)
-	}
-	// Does the user want to generate a new wallet address?
-	if options.genAddress {
-		panic("genAddress features is now moved to github.com/skycoin/cx-chains repo")
-		// optionGenAddress(options)
-		return
-	}
-	// Does the user want to generate a new wallet address?
-	if options.walletMode {
-		panic("genWallet features is now moved to github.com/skycoin/cx-chains repo")
-		// optionGenWallet(options)
-		return
-	}
-
 	if checkHelp(args) {
 		commandLine.PrintDefaults()
 		return
@@ -284,44 +267,12 @@ func Run(args []string) {
 	}
 }
 
-// mergeBlockchainHeap adds the heap `bcHeap` found in the program state of a CX
-// chain to the program to be run `PRGRM` and updates all the references to heap
-// objects found in the transaction code considering the data segment found in
-// the serialized program `sPrgrm`.
-func mergeBlockchainHeap(bcHeap, sPrgrm []byte) {
-	// Setting the CX runtime to run `PRGRM`.
-	actions.PRGRM.SelectProgram()
-
-	bcHeapLen := len(bcHeap)
-	remHeapSpace := len(actions.PRGRM.Memory[actions.PRGRM.HeapStartsAt:])
-	fullDataSegSize := actions.PRGRM.HeapStartsAt - actions.PRGRM.StackSize
-	// Copying blockchain code heap.
-	if bcHeapLen > remHeapSpace {
-		// We don't have enough space. We're using the available bytes...
-		for c := 0; c < remHeapSpace; c++ {
-			actions.PRGRM.Memory[actions.PRGRM.HeapStartsAt+c] = bcHeap[c]
-		}
-		// ...and then we append the remaining bytes.
-		actions.PRGRM.Memory = append(actions.PRGRM.Memory, bcHeap[remHeapSpace:]...)
-	} else {
-		// We have enough space and we simply write the bytes.
-		for c := 0; c < bcHeapLen; c++ {
-			actions.PRGRM.Memory[actions.PRGRM.HeapStartsAt+c] = bcHeap[c]
-		}
-	}
-	// Recalculating the heap size.
-	actions.PRGRM.HeapSize = len(actions.PRGRM.Memory) - actions.PRGRM.HeapStartsAt
-	txnDataLen := fullDataSegSize - cxcore.GetSerializedDataSize(sPrgrm)
-	// TODO: CX chains only work with one package at the moment (in the blockchain code). That is what that "1" is for.
-	// Displacing the references to heap objects by `txnDataLen`.
-	// This needs to be done as the addresses to the heap objects are displaced
-	// by the addition of the transaction code's data segment.
-	cxcore.DisplaceReferences(actions.PRGRM, txnDataLen, 1)
-}
-
 // Used for the -heap-initial, -heap-max and -stack-size flags.
 // This function parses, for example, "1M" to 1048576 (the corresponding number of bytes)
 // Possible suffixes are: G or g (gigabytes), M or m (megabytes), K or k (kilobytes)
+
+//IS NOT CALLED BY ANYTHING
+/*
 func parseMemoryString(s string) int {
 	suffix := s[len(s)-1]
 	_, notSuffix := strconv.ParseFloat(string(suffix), 64)
@@ -358,7 +309,7 @@ func parseMemoryString(s string) int {
 		}
 	}
 }
-
+*/
 func unsafeEval(code string) (out string) {
 	var lexer *parser.Lexer
 	defer func() {
