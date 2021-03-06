14a21dee8218cdb30fef9a238235144cf40f24fb
PratikDhanave <i.pratikdhanave@gmail.com>
2021-03-06 19:55:53 +0530

    root casue analysis

--- cx/op_und.go
+++ cx/op_und.go
@@ -3,6 +3,7 @@ package cxcore
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -361,6 +362,7 @@ func opAdd(expr *CXExpression, fp int) {
 	case TYPE_F64:
 		opF64Add(expr, fp)
 	default:
+		log.Println("CX_INTERNAL_ERROR :")
 		panic(CX_INTERNAL_ERROR)
 	}
 }
