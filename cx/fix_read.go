979dbb09c333f59a1df66abfb6804916d0226ad6
SkycoinSynth <Synth@Skycoin.com>
2021-03-07 17:22:10 +0800

    moved weird read functions to new file

--- cx/fix_read.go
+++ cx/fix_read.go
@@ -4,43 +4,6 @@ import()
 
 //first section
 
-// ReadBool ...
-func ReadBool(fp int, inp *CXArgument) (out bool) {
-	offset := GetFinalOffset(fp, inp)
-	out = mustDeserializeBool(ReadMemory(offset, inp))
-	return
-}
-
-// ReadStr ...
-func ReadStr(fp int, inp *CXArgument) (out string) {
-	var offset int32
-	off := GetFinalOffset(fp, inp)
-	if inp.Name == "" {
-		// Then it's a literal.
-		offset = int32(off)
-	} else {
-		offset = mustDeserializeI32(PROGRAM.Memory[off : off+TYPE_POINTER_SIZE])
-	}
-
-	if offset == 0 {
-		// Then it's nil string.
-		out = ""
-		return
-	}
-
-	// We need to check if the string lives on the data segment or on the
-	// heap to know if we need to take into consideration the object header's size.
-	if int(offset) > PROGRAM.HeapStartsAt {
-		size := mustDeserializeI32(PROGRAM.Memory[offset+OBJECT_HEADER_SIZE : offset+OBJECT_HEADER_SIZE+STR_HEADER_SIZE])
-		mustDeserializeRaw(PROGRAM.Memory[offset+OBJECT_HEADER_SIZE:offset+OBJECT_HEADER_SIZE+STR_HEADER_SIZE+size], &out)
-	} else {
-		size := mustDeserializeI32(PROGRAM.Memory[offset : offset+STR_HEADER_SIZE])
-		mustDeserializeRaw(PROGRAM.Memory[offset:offset+STR_HEADER_SIZE+size], &out)
-	}
-
-	return out
-}
-
 //second section
 
 // ReadI8 ...
