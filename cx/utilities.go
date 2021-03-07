5ab0228b97325f5fb6f334fc9f8440efc8c0efd6
SkycoinSynth <Synth@Skycoin.com>
2021-03-07 17:01:25 +0800

    moved deserialization utility functions

--- cx/utilities.go
+++ cx/utilities.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"os"
 	"path/filepath"
 	"runtime/debug"
@@ -1092,66 +1091,6 @@ func GetPrintableValue(fp int, arg *CXArgument) string {
 	return getNonCollectionValue(fp, arg, elt, typ)
 }
 
-func mustDeserializeBool(b []byte) bool {
-	switch b[0] {
-	case 0:
-		return false
-	case 1:
-		return true
-	default:
-		panic(encoder.ErrInvalidBool)
-		//return false [2020 Jun 07 (ReewassSquared)]: does nothing
-	}
-}
-
-func mustDeserializeI8(b []byte) int8 {
-	return int8(b[0])
-}
-
-func mustDeserializeI16(b []byte) int16 {
-	return int16(b[0]) | int16(b[1])<<8
-}
-
-func mustDeserializeI32(b []byte) int32 {
-	return int32(b[0]) | int32(b[1])<<8 | int32(b[2])<<16 | int32(b[3])<<24
-}
-
-func mustDeserializeI64(b []byte) int64 {
-	return int64(b[0]) | int64(b[1])<<8 | int64(b[2])<<16 | int64(b[3])<<24 |
-		int64(b[4])<<32 | int64(b[5])<<40 | int64(b[6])<<48 | int64(b[7])<<56
-}
-
-func mustDeserializeUI8(b []byte) uint8 {
-	return uint8(b[0])
-}
-
-func mustDeserializeUI16(b []byte) uint16 {
-	return uint16(b[0]) | uint16(b[1])<<8
-}
-
-func mustDeserializeUI32(b []byte) uint32 {
-	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
-}
-
-func mustDeserializeUI64(b []byte) uint64 {
-	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
-		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
-}
-
-func mustDeserializeF32(b []byte) float32 {
-	return math.Float32frombits(mustDeserializeUI32(b))
-}
-
-func mustDeserializeF64(b []byte) float64 {
-	return math.Float64frombits(mustDeserializeUI64(b))
-}
-
-func mustDeserializeRaw(byts []byte, item interface{}) {
-	_, err := encoder.DeserializeRaw(byts, item)
-	if err != nil {
-		panic(err)
-	}
-}
 
 // DebugHeap prints the symbols that are acting as pointers in a CX program at certain point during the execution of the program along with the addresses they are pointing. Additionally, a list of the objects in the heap is printed, which shows their address in the heap, if they are marked as alive or as dead by the garbage collector, the address where they used to live after a garbage collector call, the full size of the object, the object itself as a slice of bytes and the pointers that are pointing to that object.
 func DebugHeap() {
