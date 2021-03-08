77021328f34cc16587bde0e016d93391e2b05837
SkycoinSynth <synth@skycoin.com>
2021-03-08 20:19:45 +0800

    magical miracle bug

--- cx/fix_mem3.go
+++ cx/fix_mem3.go
@@ -32,6 +32,8 @@ func GetFinalOffset(fp int, arg *CXArgument) int {
 	// elt = arg
 	//TODO: Eliminate all op codes with more than one return type
 	//TODO: Eliminate this loop
+	//Q: How can CalculateDereferences change offset?
+	//Why is finalOffset fed in as a pointer?
 	CalculateDereferences(arg, &finalOffset, fp)
 	for _, fld := range arg.Fields {
 		// elt = fld
@@ -42,59 +44,83 @@ func GetFinalOffset(fp int, arg *CXArgument) int {
 	return finalOffset
 }
 
+func GetOffsetAtomic(fp int, arg *CXArgument) int {
+	//OMFG. comment this out this line and run `make build; make test`
+	return GetFinalOffset(fp, arg)
+
+	finalOffset := arg.Offset
+	//Todo: find way to eliminate this check
+	if finalOffset < PROGRAM.StackSize {
+		// Then it's in the stack, not in data or heap and we need to consider the frame pointer.
+		finalOffset += fp
+	}
+	return finalOffset
+
+}
 // GetOffset_i8 ...
 func GetOffset_i8(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp,arg)
 }
 
 // GetOffset_i16 ...
 func GetOffset_i16(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_i32 ...
 func GetOffset_i32(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_i64 ...
 func GetOffset_i64(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_ui8 ...
 func GetOffset_ui8(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_ui16 ...
 func GetOffset_ui16(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_ui32 ...
 func GetOffset_ui32(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_ui64 ...
 func GetOffset_ui64(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_f32 ...
 func GetOffset_f32(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_f64 ...
 func GetOffset_f64(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_bool ...
 func GetOffset_bool(fp int, arg *CXArgument) int {
-	return GetFinalOffset(fp, arg)
+	//return GetFinalOffset(fp, arg)
+	return GetOffsetAtomic(fp, arg)
 }
 
 // GetOffset_str ...
