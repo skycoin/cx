01c9b5d3b6f50f57d5b0f19149b124ee8b6268e8
PratikDhanave <i.pratikdhanave@gmail.com>
2021-03-08 19:05:07 +0530

    documetation for writting package version 0.1

--- cx/opcodes.go
+++ cx/opcodes.go
@@ -7,7 +7,7 @@ import (
 // CorePackages ...
 var CorePackages = []string{
 	// temporary solution until we can implement these packages in pure CX I guess
-	"al", "gl", "glfw", "time", "http", "os", "explorer", "aff", "gltext", "cx", "json", "regexp", "cipher",
+	"al", "gl", "glfw", "time", "http", "os", "explorer", "aff", "gltext", "cx", "json", "regexp", "cipher","tcp"
 }
 
 // op codes
