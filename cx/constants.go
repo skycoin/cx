ab09d9b2fae52815b011cde4a7fa2164a087c203
SkycoinSynth <Synth@Skycoin.com>
2021-03-24 20:02:50 +0800

    moved globals to globals file

--- cx/constants.go
+++ cx/constants.go
@@ -4,27 +4,6 @@ import (
 	"os"
 )
 
-// Initializing `CXProgram` structure where packages, structs, functions and
-// global variables that belong to core packages are stored.
-func init() {
-	prgrm := CXProgram{Packages: make([]*CXPackage, 0)}
-	PROGRAM = &prgrm
-}
-
-var InREPL bool = false
-var FoundCompileErrors bool
-
-const DBG_GOLANG_STACK_TRACE = true
-
-//PROGRAM GLOBALS SHOULD NOT BE IN THIS FILE
-// global reference to our program
-var PROGRAM *CXProgram //Why do we have global?
-
-var CXPATH = os.Getenv("CXPATH") + "/"
-var BINPATH = CXPATH + "bin/" // TODO @evanlinjin: Not used.
-var PKGPATH = CXPATH + "pkg/" // TODO @evanlinjin: Not used.
-var SRCPATH = CXPATH + "src/"
-
 // var COREPATH = ""
 
 const STACK_OVERFLOW_ERROR = "stack overflow"
