ba2b90bb64bcf202c1c36160be71e358939d13a9
SkycoinSynth <Synth@Skycoin.com>
2021-03-28 12:03:35 +0800

    renaming constant types

--- cx/param.go
+++ cx/param.go
@@ -35,48 +35,6 @@ func ParamEx(paramData ParamData) *ast.CXArgument {
 	return arg
 }
 
-// AI8 Default i8 parameter
-var AI8 = ast.NewCXArgument(constants.TYPE_I8)
-
-// AI16 Default i16 parameter
-var AI16 = ast.NewCXArgument(constants.TYPE_I16)
-
-// AI32 Default i32 parameter
-var AI32 = ast.NewCXArgument(constants.TYPE_I32)
-
-// AI64 Default i64 parameter
-var AI64 = ast.NewCXArgument(constants.TYPE_I64)
-
-// AUI8 Default ui8 parameter
-var AUI8 = ast.NewCXArgument(constants.TYPE_UI8)
-
-// AUI16 Default ui16 parameter
-var AUI16 = ast.NewCXArgument(constants.TYPE_UI16)
-
-// AUI32 Default ui32 parameter
-var AUI32 = ast.NewCXArgument(constants.TYPE_UI32)
-
-// AUI64 Default ui64 parameter
-var AUI64 = ast.NewCXArgument(constants.TYPE_UI64)
-
-// AF32 Default f32 parameter
-var AF32 = ast.NewCXArgument(constants.TYPE_F32)
-
-// AF64 Default f64 parameter
-var AF64 = ast.NewCXArgument(constants.TYPE_F64)
-
-// ASTR Default str parameter
-var ASTR = ast.NewCXArgument(constants.TYPE_STR)
-
-// ABOOL Default bool parameter
-var ABOOL = ast.NewCXArgument(constants.TYPE_BOOL)
-
-// AUND Default und parameter
-var AUND = ast.NewCXArgument(constants.TYPE_UNDEFINED)
-
-// AAFF Default aff parameter
-var AAFF = ast.NewCXArgument(constants.TYPE_AFF)
-
 // In Returns a slice of arguments from an argument list
 func In(params ...*ast.CXArgument) []*ast.CXArgument {
 	return params
