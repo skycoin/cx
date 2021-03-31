148b66a8e1e70ca1407b8ab7b716d71a11ee4b50
Kenje Hofileña <kenje4090@gmail.com>
2021-03-31 23:30:33 +0800

    Add comments on expressions

--- cx/astapi/expressions.go
+++ cx/astapi/expressions.go
@@ -4,6 +4,7 @@ import (
 	cxast "github.com/skycoin/cx/cx/ast"
 )
 
+// AddNativeExpressionToFunction adds a native expression to a function in cx program.
 func AddNativeExpressionToFunction(cxprogram *cxast.CXProgram, functionName string, expressionType int) error {
 	exp := cxast.MakeExpression(cxast.Natives[expressionType], "", -1)
 	exp.Operator.Name = cxast.OpNames[expressionType]
@@ -17,6 +18,7 @@ func AddNativeExpressionToFunction(cxprogram *cxast.CXProgram, functionName stri
 	return nil
 }
 
+// RemoveExpressionFromFunction removes an expression from function in cx program.
 func RemoveExpressionFromFunction(cxprogram *cxast.CXProgram, functionName string, expressionLineNumber int) error {
 	fn, err := FindFunction(cxprogram, functionName)
 	if err != nil {
@@ -27,6 +29,8 @@ func RemoveExpressionFromFunction(cxprogram *cxast.CXProgram, functionName strin
 	return nil
 }
 
+// AddNativeExpressionToFunctionByLineNumber adds a native expression
+// on a specific N line number of a function in cx program.
 func AddNativeExpressionToFunctionByLineNumber(cxprogram *cxast.CXProgram, functionName string, expressionType, lineNumber int) error {
 	exp := cxast.MakeExpression(cxast.Natives[expressionType], "", -1)
 	exp.Operator.Name = cxast.OpNames[expressionType]
