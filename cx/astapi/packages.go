942fd89385278a3e416665fd4e1487a14724f08a
Kenje Hofileña <kenje4090@gmail.com>
2021-03-31 23:26:50 +0800

    Add comments on packages

--- cx/astapi/packages.go
+++ cx/astapi/packages.go
@@ -4,6 +4,7 @@ import (
 	cxast "github.com/skycoin/cx/cx/ast"
 )
 
+// GetPackagesNameList returns all names of all packages in cx program.
 func GetPackagesNameList(cxprogram *cxast.CXProgram) (list []string) {
 	for _, pkg := range cxprogram.Packages {
 		list = append(list, pkg.Name)
@@ -11,6 +12,7 @@ func GetPackagesNameList(cxprogram *cxast.CXProgram) (list []string) {
 	return list
 }
 
+// AddEmptyPackage adds an empty package in cx program.
 func AddEmptyPackage(cxprogram *cxast.CXProgram, pkgName string) error {
 	pkg := cxast.MakePackage(pkgName)
 	cxprogram.AddPackage(pkg)
