7fdc80f7ca55b739373756e3d5dd92c7e223457d
PratikDhanave <i.pratikdhanave@gmail.com>
2021-05-16 20:41:51 +0530

    ast doc

--- cxparser/prattparser/docs.go
+++ cxparser/prattparser/docs.go
@@ -63,10 +63,29 @@
 	In the pratt parsing we construct ast as well as evaluate operator precedence.
 
 
-	for every grammer/ token we have function assocated with to constuct AST.
+	for every grammer/ token we have functions assocated with to constuct AST node.
+
+	for example : parseIntegerLiteral ()
 
 	How to write Parsing
 
+
+
+References :
+
+i) https://engineering.desmos.com/articles/pratt-parser/
+ii)https://matklad.github.io/2020/04/13/simple-but-powerful-pratt-parsing.html
+iii) http://craftinginterpreters.com/parsing-expressions.html
+iv) https://www.oilshell.org/blog/2017/03/31.html
+v) http://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/
+vi) https://en.wikipedia.org/wiki/Operator-precedence_parser
+vii) https://www.oilshell.org/blog/2016/11/01.html
+viii) https://www.npmjs.com/package/pratt-parser
+ix) https://eli.thegreenplace.net/2010/01/02/top-down-operator-precedence-parsing
+x) https://dev.to/jrop/pratt-parsing
+xi) https://github.com/jrop/pratt-calculator
+xii) https://crockford.com/javascript/tdop/tdop.html
+xiii) https://elementpath.readthedocs.io/en/latest/pratt_api.html
 
 */
 
