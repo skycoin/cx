f4e3446ac198d5c02051f40afcbe7c127f71bf65
Jeremy Chang <jermybz01@gmail.com>
2022-06-20 11:49:23 -0600

    setup

---
+++ cmd/type_checks/type_checks.go
@@ -0,0 +1,21 @@
+package type_checks
+
+import (
+	"github.com/skycoin/cx/cmd/declaration_extraction"
+)
+
+func PraseGlobals(globals []declaration_extraction.GlobalDeclaration) {
+
+}
+
+func ParseEnums(enums []declaration_extraction.EnumDeclaration) {
+
+}
+
+func ParseStructs(structs []declaration_extraction.StructDeclaration) {
+
+}
+
+func ParseFuncs(funcs []declaration_extraction.FuncDeclaration) {
+
+}
