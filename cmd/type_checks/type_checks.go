3571d2dd669cb0ccfcacc390324dc832bf2be8f2
Jeremy Chang <jermybz01@gmail.com>
2022-06-20 16:05:25 -0600

    add type structs

--- cmd/type_checks/type_checks.go
+++ cmd/type_checks/type_checks.go
@@ -4,8 +4,28 @@ import (
 	"github.com/skycoin/cx/cmd/declaration_extraction"
 )
 
-func PraseGlobals(globals []declaration_extraction.GlobalDeclaration) {
+type GlobalType struct {
+	Name string
+	Type string
+}
 
+type EnumType struct {
+	Name string
+	Type string
+}
+
+type StructType struct {
+	Name string
+	Type string
+}
+
+func ParseGlobals(globals []declaration_extraction.GlobalDeclaration) ([]GlobalType, error) {
+
+	var GlobalTypes []GlobalType
+
+	for _, global := range globals {
+
+	}
 }
 
 func ParseEnums(enums []declaration_extraction.EnumDeclaration) {
--- cmd/type_checks/type_checks_test.go
+++ cmd/type_checks/type_checks_test.go
@@ -1,1 +1,63 @@
 package type_checks_test
+
+import "testing"
+
+func TestTypeChecks_ParseGlobals(t *testing.T) {
+
+	tests := []struct {
+		scenario string
+		testDir  string
+	}{}
+
+	for _, tc := range tests {
+		t.Run(tc.scenario, func(t *testing.T) {
+
+		})
+	}
+
+}
+
+func TestTypeChecks_ParseEnums(t *testing.T) {
+
+	tests := []struct {
+		scenario string
+		testDir  string
+	}{}
+
+	for _, tc := range tests {
+		t.Run(tc.scenario, func(t *testing.T) {
+
+		})
+	}
+
+}
+
+func TestTypeChecks_ParseStructs(t *testing.T) {
+
+	tests := []struct {
+		scenario string
+		testDir  string
+	}{}
+
+	for _, tc := range tests {
+		t.Run(tc.scenario, func(t *testing.T) {
+
+		})
+	}
+
+}
+
+func TestTypeChecks_ParseFuncs(t *testing.T) {
+
+	tests := []struct {
+		scenario string
+		testDir  string
+	}{}
+
+	for _, tc := range tests {
+		t.Run(tc.scenario, func(t *testing.T) {
+
+		})
+	}
+
+}
