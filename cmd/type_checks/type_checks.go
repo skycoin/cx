e687c499f7551470a790dd7d4e5f81b6f5efc147
Jeremy <jermybz01@gmail.com>
2022-06-22 21:16:07 -0600

    add comments for next step

--- cmd/type_checks/type_checks.go
+++ cmd/type_checks/type_checks.go
@@ -13,11 +13,17 @@ import (
 
 func ParseGlobals(globals []declaration_extraction.GlobalDeclaration, program *ast.CXProgram) {
 
+	// 1. interate over all the globals
+	// 2. open file once and add to an array of src's
+	// 3. if src is in array already don't open again
+	// 4. extract the type and initializations in src
+	// 5. add all the over information to CXProgram
+
 	for _, global := range globals {
 
 		pkg, err := program.GetPackage(global.PackageID)
 		declarator := ast.MakeArgument(global.GlobalVariableName, global.FileID, global.LineNumber)
-		// declarationSpecifiers := ast.MakeArgument()
+		declarationSpecifiers := actions.DeclarationSpecifiers()
 		// initializer := ast.MakeCXLineExpression(&program, global.FileID, global.LineNumber)
 
 		if err != nil {
@@ -37,13 +43,22 @@ func ParseGlobals(globals []declaration_extraction.GlobalDeclaration, program *a
 
 func ParseEnums(enums []declaration_extraction.EnumDeclaration) {
 
+	// Not sure about this one
+
+	for _, enum := range enums {
+
+		actions.de
+	}
 }
 
 func ParseStructs(structs []declaration_extraction.StructDeclaration, program *ast.CXProgram) {
 
-	for _, strct := range structs {
+	// 1. iterate over all the structs
+	// 2. add the struct name from the declaration
+	// 3. search for fields
+	// 4. fields to ast
 
-		declarator := ast.MakeArgument(strct.StructVariableName, strct.FileID, strct.LineNumber)
+	for _, strct := range structs {
 
 		src, err := os.Open(strct.FileID)
 
@@ -56,7 +71,7 @@ func ParseStructs(structs []declaration_extraction.StructDeclaration, program *a
 		scanner = bufio.NewScanner(src)
 
 		for scanner.Scan() {
-			line := scanner.Scanner
+			line := scanner
 		}
 
 		actions.DeclareStruct(program, strct.StructVariableName, nil)
@@ -64,18 +79,21 @@ func ParseStructs(structs []declaration_extraction.StructDeclaration, program *a
 	}
 }
 
-func ParseFuncs(funcs []declaration_extraction.FuncDeclaration) {
+func ParseFuncs(funcs []declaration_extraction.FuncDeclaration, program *ast.CXProgram) {
+
+	// 1. iterate over all the funcs
+	// 2. extract inputs and outputs
+	// 3. get the id and expression
+	// 4. call function declaration
 
 	for _, fun := range funcs {
 
-		pkg, err := program.GetPackage(fun.PackageID)
 		declarator := ast.MakeArgument(fun.FuncVariableName, fun.FileID, fun.LineNumber)
 
-		if err != nil {
-			pkg = ast.MakePackage(fun.PackageID)
-		}
+		src, err := os.Open(fun.FileID)
 
-		actions.FunctionDeclaration(&program)
+		actions.FunctionDeclaration(program)
+
 	}
 
 }
