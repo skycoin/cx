5dbd5e90a3789b8f64a8344497989ba9eb1f433f
Jeremy <jermybz01@gmail.com>
2022-07-04 14:06:42 -0600

    rewrite test

--- cmd/type_checks/type_checks.go
+++ cmd/type_checks/type_checks.go
@@ -19,14 +19,21 @@ var primitiveTypesMap map[string]types.Code = map[string]types.Code{
 
 	"bool": types.BOOL,
 
-	"byte": types.I8,
-	"i32":  types.I32,
-	"i64":  types.I64,
+	"i8":  types.I8,
+	"i16": types.I16,
+	"i32": types.I32,
+	"i64": types.I64,
+
+	"ui8":  types.UI8,
+	"ui16": types.UI16,
+	"ui32": types.UI32,
+	"ui64": types.UI64,
 
 	"f32": types.F32,
 	"f64": types.F64,
 
 	"str": types.STR,
+	"aff": types.AFF,
 }
 
 // ParseGlobals make the CXArguments and add them to the AST
@@ -89,7 +96,7 @@ func ParseGlobals(globals []declaration_extraction.GlobalDeclaration) {
 
 		pkg.AddGlobal(actions.AST, globalArgIdx)
 
-		globalArg.DeclarationSpecifiers = actions.DeclareGlobalInPackage()
+		// globalArg.DeclarationSpecifiers = actions.DeclareGlobalInPackage()
 
 	}
 }
