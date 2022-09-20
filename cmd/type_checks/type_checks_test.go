22a5351179bd517610b332959d0ee5696180ae6e
Jeremy Chang <jermybz01@gmail.com>
2022-09-20 16:13:37 -0600

    complete ParseGlobals unit test

--- cmd/type_checks/type_checks_test.go
+++ cmd/type_checks/type_checks_test.go
@@ -248,110 +248,117 @@ func TestTypeCheck_ParseParameterDeclaration(t *testing.T) {
 
 func TestTypeChecks_ParseGlobals(t *testing.T) {
 
+	type GlobalTypeSignature struct {
+		Package string
+		Index   int
+		Name    string
+		Type    string
+	}
+
 	tests := []struct {
-		scenario      string
-		testDir       string
-		globalsCXArgs []*ast.CXArgument
+		scenario             string
+		testDir              string
+		globalTypeSignatures []GlobalTypeSignature
 	}{
 		{
 			scenario: "Has globals",
-			testDir:  "./test_files/test.cx",
-			globalsCXArgs: []*ast.CXArgument{
+			testDir:  "./test_files/ParseGlobals/test.cx",
+			globalTypeSignatures: []GlobalTypeSignature{
 				{
 					Name:    "Bool",
 					Index:   0,
-					Package: 1,
-					Type:    types.BOOL,
+					Package: "main",
+					Type:    "bool",
 				},
 				{
 					Name:    "Byte",
 					Index:   1,
-					Package: 1,
-					Type:    types.I8,
+					Package: "main",
+					Type:    "i8",
 				},
 				{
 					Name:    "I16",
 					Index:   2,
-					Package: 1,
-					Type:    types.I16,
+					Package: "main",
+					Type:    "i16",
 				},
 				{
 					Name:    "I32",
 					Index:   3,
-					Package: 1,
-					Type:    types.I32,
+					Package: "main",
+					Type:    "i32",
 				},
 				{
 					Name:    "I64",
 					Index:   4,
-					Package: 1,
-					Type:    types.I64,
+					Package: "main",
+					Type:    "i64",
 				},
 				{
 					Name:    "UByte",
 					Index:   5,
-					Package: 1,
-					Type:    types.UI8,
+					Package: "main",
+					Type:    "ui8",
 				},
 				{
 					Name:    "UI16",
 					Index:   6,
-					Package: 1,
-					Type:    types.UI16,
+					Package: "main",
+					Type:    "ui16",
 				},
 				{
 					Name:    "UI32",
 					Index:   7,
-					Package: 1,
-					Type:    types.UI32,
+					Package: "main",
+					Type:    "ui32",
 				},
 				{
 					Name:    "UI64",
 					Index:   8,
-					Package: 1,
-					Type:    types.UI64,
+					Package: "main",
+					Type:    "ui64",
 				},
 				{
 					Name:    "F32",
 					Index:   9,
-					Package: 1,
-					Type:    types.F32,
+					Package: "main",
+					Type:    "f32",
 				},
 				{
 					Name:    "F64",
 					Index:   10,
-					Package: 1,
-					Type:    types.F64,
+					Package: "main",
+					Type:    "f64",
 				},
 				{
 					Name:    "string",
 					Index:   11,
-					Package: 1,
-					Type:    23,
+					Package: "main",
+					Type:    "str",
 				},
 				{
 					Name:    "Affordance",
 					Index:   12,
-					Package: 1,
-					Type:    25,
+					Package: "main",
+					Type:    "[]aff",
 				},
 				{
 					Name:    "intArray",
 					Index:   13,
-					Package: 1,
-					Type:    types.I32,
+					Package: "main",
+					Type:    "[5]i32",
 				},
 			},
 		},
 		{
 			scenario: "Has globals 2",
-			testDir:  "./test_files/testFile.cx",
-			globalsCXArgs: []*ast.CXArgument{
+			testDir:  "./test_files/ParseGlobals/testFile.cx",
+			globalTypeSignatures: []GlobalTypeSignature{
 				{
 					Name:    "number",
 					Index:   0,
-					Package: 1,
-					Type:    types.I32,
+					Package: "main",
+					Type:    "i32",
 				},
 			},
 		},
@@ -359,17 +366,6 @@ func TestTypeChecks_ParseGlobals(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.scenario, func(t *testing.T) {
-
-			// actions.AST = cxinit.MakeProgram()
-			// var files []*os.File
-			// file, err := os.Open(tc.testDir)
-			// if err != nil {
-			// 	t.Fatal()
-			// }
-			// files = append(files, file)
-			// cxparsering.ParseSourceCode(files, []string{tc.testDir})
-
-			// actions.AST.PrintProgram()
 
 			actions.AST = nil
 
@@ -396,11 +392,12 @@ func TestTypeChecks_ParseGlobals(t *testing.T) {
 
 			program := actions.AST
 
-			for _, wantGlobal := range tc.globalsCXArgs {
+			for _, wantGlobal := range tc.globalTypeSignatures {
 
 				for _, pkgIdx := range program.Packages {
 
 					pkg, err := program.GetPackageFromArray(pkgIdx)
+					gotPkgName := pkg.Name
 
 					if err != nil {
 						t.Log(err)
@@ -412,15 +409,22 @@ func TestTypeChecks_ParseGlobals(t *testing.T) {
 
 					var match bool
 					var gotGlobal *ast.CXTypeSignature
+					var gotGlobalType string
 
 					for _, globalIdx := range pkg.Globals.Fields {
 						gotGlobal = program.GetCXTypeSignatureFromArray(globalIdx)
 
 						if gotGlobal.Name == wantGlobal.Name {
 
+							if gotGlobal.Type == ast.TYPE_CXARGUMENT_DEPRECATE {
+								gotGlobalType = ast.GetFormattedType(actions.AST, actions.AST.GetCXArg(ast.CXArgumentIndex(gotGlobal.Meta)))
+							} else {
+								gotGlobalType = types.Code(gotGlobal.Meta).Name()
+							}
+
 							if int(gotGlobal.Index) == wantGlobal.Index &&
-								gotGlobal.Package == wantGlobal.Package &&
-								types.Code(gotGlobal.Meta) == wantGlobal.Type {
+								gotPkgName == wantGlobal.Package &&
+								gotGlobalType == wantGlobal.Type {
 								match = true
 							}
 
@@ -432,9 +436,9 @@ func TestTypeChecks_ParseGlobals(t *testing.T) {
 					if !match {
 
 						if gotGlobal.Type == ast.TYPE_CXARGUMENT_DEPRECATE {
-							t.Errorf("want global %v %v %v %v, got %v %v %v %v", wantGlobal.Name, wantGlobal.Index, wantGlobal.Package, wantGlobal.Type.Name(), program.GetCXArg(ast.CXArgumentIndex(gotGlobal.Meta)).Name, gotGlobal.Index, gotGlobal.Package, ast.GetFormattedType(program, program.GetCXArg(ast.CXArgumentIndex(gotGlobal.Meta))))
+							t.Errorf("want global %v %v %v %v, got %v %v %v %v", wantGlobal.Name, wantGlobal.Index, wantGlobal.Package, wantGlobal.Type, program.GetCXArg(ast.CXArgumentIndex(gotGlobal.Meta)).Name, gotGlobal.Index, gotPkgName, gotGlobalType)
 						} else {
-							t.Errorf("want global %v %v %v %v, got %v %v %v %v", wantGlobal.Name, wantGlobal.Index, wantGlobal.Package, wantGlobal.Type.Name(), gotGlobal.Name, gotGlobal.Index, gotGlobal.Package, types.Code(gotGlobal.Meta).Name())
+							t.Errorf("want global %v %v %v %v, got %v %v %v %v", wantGlobal.Name, wantGlobal.Index, wantGlobal.Package, wantGlobal.Type, gotGlobal.Name, gotGlobal.Index, gotPkgName, gotGlobalType)
 						}
 					}
 				}
