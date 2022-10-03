f80660a4b70f0189148339172961a275adc40552
Kenje Hofileña <kenje4090@gmail.com>
2022-10-03 15:58:44 +0800

    Add fix for bubble sort error

--- cxparser/actions/expressions.go
+++ cxparser/actions/expressions.go
@@ -739,6 +739,14 @@ func AssociateReturnExpressions(prgrm *ast.CXProgram, idx int, retExprs []ast.CX
 		newCXTypeSig.Meta = int(fnOutputTypeSig.Type)
 		newCXTypeSig.Offset = fnOutputTypeSig.Offset
 		typeSigIdx = prgrm.AddCXTypeSignatureInArray(newCXTypeSig)
+	} else if fnOutputTypeSig.Type == ast.TYPE_SLICE_ATOMIC {
+		newCXTypeSig := &ast.CXTypeSignature{}
+		newCXTypeSig.Name = fnOutputTypeSig.Name
+		newCXTypeSig.Package = fnOutputTypeSig.Package
+		newCXTypeSig.Type = ast.TYPE_SLICE_ATOMIC
+		newCXTypeSig.Meta = fnOutputTypeSig.Meta
+		newCXTypeSig.Offset = fnOutputTypeSig.Offset
+		typeSigIdx = prgrm.AddCXTypeSignatureInArray(newCXTypeSig)
 	} else {
 		panic("type is not known")
 	}
