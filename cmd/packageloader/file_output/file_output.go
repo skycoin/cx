c85487cff621ce4105b887557e0574653c3f8fea
Jeremy Chang <jermybz01@gmail.com>
2022-10-09 17:02:02 -0600

    get filenames and add test case

--- cmd/packageloader/file_output/file_output.go
+++ cmd/packageloader/file_output/file_output.go
@@ -1,15 +1,28 @@
 package file_output
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"os"
+	"strings"
+
+	cxinit "github.com/skycoin/cx/cx/init"
+	"github.com/skycoin/cx/cxparser/actions"
 
 	"github.com/skycoin/cx/cmd/packageloader/bolt"
 	"github.com/skycoin/cx/cmd/packageloader/loader"
 	"github.com/skycoin/cx/cmd/packageloader/redis"
 )
 
+// - Adds Imports to AST
+// - Returns Import Files
 func GetImportFiles(packageName string, database string) (files []*os.File, err error) {
+
+	if actions.AST == nil {
+		actions.AST = cxinit.MakeProgram()
+	}
+
 	var packageList loader.PackageList
 	listBytes, err := GetStructBytes(packageName, database)
 	if err != nil {
@@ -29,6 +42,7 @@ func GetImportFiles(packageName string, database string) (files []*os.File, err 
 		}
 
 		for _, fileString := range packageStruct.Files {
+
 			var fileStruct loader.File
 			fileBytes, err := GetStructBytes(fileString, database)
 			if err != nil {
@@ -36,8 +50,41 @@ func GetImportFiles(packageName string, database string) (files []*os.File, err 
 			}
 			fileStruct.UnmarshalBinary(fileBytes)
 
+			fmt.Print(fileStruct.FileName)
+
+			file, err := os.Open(fileStruct.FileName)
+			if err != nil {
+				return files, err
+			}
+
+			files = append(files, file)
+
+			scanner := bufio.NewScanner(strings.NewReader(string(fileStruct.Content)))
+			scanner.Split(bufio.ScanWords)
+
+			wordBefore := ""
+			for scanner.Scan() {
+
+				if scanner.Text() != "import" {
+					wordBefore = scanner.Text()
+					continue
+				}
+				if wordBefore == "//" {
+					wordBefore = scanner.Text()
+					continue
+				}
+				if scanner.Text() == "var" || scanner.Text() == "const" || scanner.Text() == "type" || scanner.Text() == "func" {
+					break
+				}
+				scanner.Scan()
+				importString := scanner.Text()[1 : len(scanner.Text())-1]
+				actions.DeclareImport(actions.AST, importString, fileString, 0)
+				wordBefore = scanner.Text()
+			}
+
 		}
 	}
+
 	return files, nil
 }
 
--- cmd/packageloader/file_output/file_output_test.go
+++ cmd/packageloader/file_output/file_output_test.go
@@ -1,15 +1,40 @@
 package file_output_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/skycoin/cx/cmd/packageloader/file_output"
+	"github.com/skycoin/cx/cmd/packageloader/loader"
+)
 
 func TestFile_Output_GetImportFiles(t *testing.T) {
 	tests := []struct {
-		scenario string
-	}{}
+		scenario    string
+		programName string
+		testDir     string
+		database    string
+	}{
+		{
+			scenario:    "Has Imports",
+			programName: "tester",
+			testDir:     "./test_files/test_valid_program",
+			database:    "bolt",
+		},
+	}
 
 	for _, tc := range tests {
 		t.Run(tc.scenario, func(t *testing.T) {
+			_, sourceCodes, _ := loader.ParseArgsForCX([]string{tc.testDir}, true)
+			err := loader.LoadCXProgram(tc.programName, sourceCodes, tc.database)
+			if err != nil {
+				t.Fatal(err)
+			}
+			files, err := file_output.GetImportFiles(tc.programName, tc.database)
+			if err != nil {
+				t.Fatal(err)
+			}
 
+			t.Error(files)
 		})
 	}
 }
