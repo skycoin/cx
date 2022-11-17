a2698a3e599ebb90bc5ed50a73709a4fc6e2c824
Jeremy Chang <jermybz01@gmail.com>
2022-11-17 17:49:55 -0600

    move variables

--- cmd/packageloader2/loader/loader.go
+++ cmd/packageloader2/loader/loader.go
@@ -18,6 +18,10 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
+var SKIP_PACKAGES = []string{"al", "gl", "glfw", "time", "os", "gltext", "cx", "json", "cipher", "tcp"}
+var FileHashMap = make(map[string]string)
+var PackageHashMap = make(map[string]string)
+
 func ParseArgsForCX(args []string, alsoSubdirs bool) (cxArgs []string, sourceCode []*os.File, fileNames []string) {
 	skip := false // flag for skipping arg
 
--- cmd/packageloader2/loader/variables.go
+++
@@ -1,5 +0,0 @@
-package loader
-
-var SKIP_PACKAGES = []string{"al", "gl", "glfw", "time", "os", "gltext", "cx", "json", "cipher", "tcp"}
-var FileHashMap = make(map[string]string)
-var PackageHashMap = make(map[string]string)
